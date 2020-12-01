02d77ac0984a4ac0267b3d02aa594b4a57082f50
7h3730B <56200898+7h3730B@users.noreply.github.com>
2020-12-01 21:28:26 +0100

    just testing

--- 2020/01/02/main.py
+++ 2020/01/02/main.py
@@ -4,6 +4,7 @@ with open("input.txt", "r") as inputValues:
     inputs = inputValues.read().split('\n')
     for i in inputs:
         listOfInputs.append(int(i))
+        
 
 res = [i * j * k for i in listOfInputs for j in listOfInputs for k in listOfInputs if i + j + k == 2020]
 print(f'The solution is: {res[0]}')  # 173538720
