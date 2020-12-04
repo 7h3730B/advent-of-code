8af19f4184084841b95e9267cae84cd67c4c5e59
7h3730B <teoschulze5@gmail.com>
2020-12-04 15:41:28 +0100

    2020-03-01 completed

---
+++ 2020/03/main.py
@@ -0,0 +1,20 @@
+inputValues = []
+x = 0
+y = 0
+
+with open('input.txt', 'r') as inputVal:
+    for i in inputVal.readlines():
+        inputValues.append(i.replace('\n', ''))
+
+trees = 0
+for i in inputValues:
+    x += 3
+    y += 1
+    if x >= len(i):
+        x = (x - len(i))
+        print(x)
+    if y > 322:
+        print(trees) # 205
+        exit(0)
+    if inputValues[y][x] == '#':
+        trees += 1
